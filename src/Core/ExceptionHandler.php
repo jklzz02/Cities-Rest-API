61b928466f5e6ecdf437e3c01be3f673e5296218
jklzz02 <jacopo.lazzaro.jl@gmail.com>
2024-11-18 10:15:10 +0100

    error handler

---
+++ src/Core/ExceptionHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Jklzz02\RestApi\Core;
+
+class ExceptionHandler{
+
+    protected Response $response;
+
+    public function __construct(Response $response)
+    {
+        $this->response = $response;
+    }
+
+    public function handle(\Throwable $e) : never
+    {
+        error_log($e);
+
+        switch (get_class($e)) {
+
+            case \InvalidArgumentException::class:
+                $this->response->badRequest();
+                break;
+
+            default:
+                $this->response->internalError();
+                break;
+        }
+    }
+
+}
