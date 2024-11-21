10164a0e65aac25cb1153499ed3e7a3e4ee3f934
jklzz02 <jacopo.lazzaro.jl@gmail.com>
2024-11-21 10:40:40 +0100

    Auth update

--- src/Middleware/Auth.php
+++ src/Middleware/Auth.php
@@ -5,26 +5,22 @@ namespace Jklzz02\RestApi\Middleware;
 
 use Jklzz02\RestApi\Core\Database;
 use Jklzz02\RestApi\Core\Request;
-use Jklzz02\RestApi\Core\Responder;
 use Jklzz02\RestApi\Exception\HTTPException\HTTPUnauthorizedException;
 use PDO;
 
 class Auth{
 
     protected PDO $connection;
-    protected string $token;
-    protected Responder $responder;
 
-    public function __construct(Request $request, Database $db)
+    public function __construct(protected Request $request, Database $db)
     {
         $this->connection = $db->getConnection();
-        $this->token = $request->token();
     }
 
     public function handle(): void
     {
         $stmt = $this->connection->prepare("SELECT * FROM tokens WHERE token = :token");
-        $stmt->execute([":token" => $this->token]);
+        $stmt->execute([":token" => $this->request->token()]);
 
         if(!$stmt->fetch()){
             throw new HTTPUnauthorizedException();
